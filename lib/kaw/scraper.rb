651a834cce6b758521e8959e7b97176c18d62323
BekahHW <rhawrot@gmail.com>
2018-08-17 10:45:41 -0400

    created second scrape method

--- lib/kaw/scraper.rb
+++ lib/kaw/scraper.rb
@@ -20,4 +20,14 @@ def self.scrape_heroines
   end
 end
 
+def self.scrape_indiv_movies
+  Kaw::Filmography.all.each do |movie|
+    doc = Nokogiri::HTML(open(movie_url))
+
+    doc.css(".panel-body content_body").each do |content|
+
+      info = content.css("movie_synopsis clamp clamp-6")
+      ##need to figure out how to scrape data that's generically labeled
+      screenwriter = content.css(".li metarow clearfix meta-value")
+      director = content.css(".li metarow clearfix meta-value")
 end
